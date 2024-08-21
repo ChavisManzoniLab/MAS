ca7c611b1194d1e6de5c57d4c4ac7812cd510bc3
bs <cerilam@hotmail.fr>
2024-08-21 11:51:23 +0200

    fix

--- MAS/__init__.py
+++ MAS/__init__.py
@@ -1,9 +1,9 @@
-from MAS.config import FIRST_START, ROOT
-
+from MAS.config import  ROOT
+import os
 
 print("CHECKING IF MODELS ARE DOWNLOADED")
 
-if FIRST_START:
+if not os.path.isdir(ROOT + 'Models'):
 	import zipfile
 	import gdown
 	url = 'https://drive.google.com/uc?id=1PNR0ztzzbm1dguC0SC7GYl6dO9EI-ya4'
