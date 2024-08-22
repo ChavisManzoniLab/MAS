c7a4898af2dc8a16c689e2d623ec2a80e5e91e8c
bs <cerilam@hotmail.fr>
2024-08-22 15:28:22 +0200

    :)

--- MAS/__init__.py
+++ MAS/__init__.py
@@ -8,14 +8,14 @@ print("GPU CHECK")
 print("███████████████████████████████████████████████████████████")
 
 if  tf.test.is_built_with_cuda():
-	print("TENSORFLOW AND GPU WORKING")
+	print("Tensorflow is working with GPU ☺")
 else :
-	("TENSORFLOW NOT WORKING WITH GPU")
+	("TENSORFLOW NOT WORKING WITH GPU ☺")
 
-print("███████████████████████████████████████████████████████████")
+print(" ")
 
 if torch.cuda.is_available():
-	print("TORCH IS WORKING WITH GPU")
+	print("Torch is working with GPU")
 else: 
 	("TORCH NOT WORKING ON GPU")
 
@@ -26,7 +26,7 @@ print("CHECKING IF MODELS ARE DOWNLOADED")
 
 
 
-if not os.path.isdir(ROOT + 'Models'):
+if not os.path.isdir(ROOT + '\Models'):
 	import zipfile
 	import gdown
 	url = 'https://drive.google.com/uc?id=1PNR0ztzzbm1dguC0SC7GYl6dO9EI-ya4'
