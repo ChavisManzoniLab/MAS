25393b78e0af82c1985880f8528bcfcdc5139ed3
Strauss Benjamin <163406503+StraussBenjamin@users.noreply.github.com>
2024-10-21 13:47:27 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx-rtd-theme"]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +23,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_static_path = ['_static']
